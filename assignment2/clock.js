de5518ebccdb34ecb45a5c05ba88d492be74ed71
YeongWoooo <rdd9223@gmail.com>
2021-04-23 21:59:39 +0900

    Feat: 24시간, 12시간 변경 적용

--- assignment2/clock.js
+++ assignment2/clock.js
@@ -5,7 +5,6 @@ const analogHour = document.querySelector(".analog_hour"); // 해당 클래스�
 const analogMinute = document.querySelector(".analog_minute"); // 해당 클래스이름을 가진 요소를 가져옵니다
 const analogSecond = document.querySelector(".analog_second"); // 해당 클래스이름을 가진 요소를 가져옵니다
 
-const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let hour24 = false; // 24시간인지 12시간인지 저장하는 전역변수를 지정합니다
 
 // 00 : 00의 형식을 맞추기 위해 한자리 수는 앞에 0을 채워주는 함수입니다
@@ -26,20 +25,31 @@ const getDate = () => {
   let hour = today.getHours();
   const minute = today.getMinutes();
   const second = today.getSeconds();
+  const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const monthName = monthList[month];
-
-  if (hour >= 0 && hour < 12) {
-    if (hour === 0) hour = 12;
-    digitalButton.innerHTML = "AM";
-  } else {
-    if (hour >= 13) hour -= 12;
-    digitalButton.innerHTML = "PM";
-  }
 
   return { today, year, month, day, hour, minute, second, monthName };
 };
 
-const renderClock = ({ year, day, hour, minute, second, monthName }) => {
+const changeTime = () => {
+  hour24 = !hour24;
+};
+
+const renderClock = () => {
+  let { today, year, month, day, hour, minute, second, monthName } = getDate();
+
+  if (!hour24) {
+    if (hour >= 0 && hour < 12) {
+      if (hour === 0) hour = 12;
+      digitalButton.innerHTML = "AM";
+    } else {
+      if (hour >= 13) hour -= 12;
+      digitalButton.innerHTML = "PM";
+    }
+  } else {
+    digitalButton.innerHTML = "24H"; // 버튼 내부의 텍스트도 바꿉니다
+  }
+
   // 시침의 각도를 계산합니다. 시침은 한 시간당 30도씩, 분당 0.5도씩 움직입니다
   // 90도를 더하는 이유는 바늘이 -90도로 누워있는 상태에서 시작하기 때문입니다 (css 참고)
   const hourDegree = (hour + minute / 60) * (360 / 12) + 90;
@@ -55,10 +65,10 @@ const renderClock = ({ year, day, hour, minute, second, monthName }) => {
 };
 
 const init = () => {
-  const now = getDate();
+  renderClock();
 
-  renderClock(now);
-  setInterval(renderClock(now), 1000); // 1초 간격으로 콜백함수를 실행합니다
+  setInterval(renderClock, 1000); // 1초 간격으로 콜백함수를 실행합니다
+  digitalButton.addEventListener("click", changeTime);
 };
 
 init();
