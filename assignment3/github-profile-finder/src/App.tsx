524de6cd00965449db3245ccb9477c9b7e053290
YeongWoooo <rdd9223@gmail.com>
2021-05-03 16:15:53 +0900

    Refactor: 메인 스타일 변경

--- assignment3/github-profile-finder/src/App.tsx
+++ assignment3/github-profile-finder/src/App.tsx
@@ -3,6 +3,7 @@ import SearchBar from "./components/SearchBar";
 import Result from "./components/Result";
 import { getUserData } from "./lib/api";
 import { IUserState } from "./interfaces";
+import Styled from "styled-components";
 
 const App = () => {
   const [userState, setUserState] = useState<IUserState>({
@@ -24,11 +25,23 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Wrapper>
       <SearchBar getUser={getUser} />
       <Result userState={userState} />
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffffff
+`;
 
 export default App;
