6e03a5c93510c04a9679425915128ac79f335afb
YeongWoooo <rdd9223@gmail.com>
2021-05-08 03:40:22 +0900

    Feat: 결과 페이지 스타일링 추가

--- assignment3/github-profile-finder/src/components/ResultCard.tsx
+++ assignment3/github-profile-finder/src/components/ResultCard.tsx
@@ -1,30 +1,83 @@
 import React from "react";
+import styled from "styled-components";
 import { IResultCardProps } from "../interfaces";
 
 const ResultCard = ({ data }: IResultCardProps) => {
   return (
-    <div>
-      <img src={data?.avatar_url} alt="user" />
-      <div>{data?.name}</div>
-      <div>{data?.login}</div>
-      <div>{data?.bio}</div>
-      <a href={data?.html_url}>Visit GitHub</a>
-      <div>
-        <div>
+    <Wrapper>
+      <Image src={data?.avatar_url} alt="user" />
+      <h2>{data?.name}</h2>
+      <p>{data?.login}</p>
+      <br />
+      <p>{data?.bio}</p>
+      <br />
+      <GithubButton href={data?.html_url}>Visit GitHub</GithubButton>
+      <InfoContainer>
+        <InfoBox>
           Followers
           <div>{data?.followers}</div>
-        </div>
-        <div>
+        </InfoBox>
+        <InfoBox>
           Following
           <div>{data?.following}</div>
-        </div>
-        <div>
+        </InfoBox>
+        <InfoBox>
           Repos
           <div>{data?.public_repos}</div>
-        </div>
-      </div>
-    </div>
+        </InfoBox>
+      </InfoContainer>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #e9e8e8;
+  width: 390px;
+  overflow: hidden;
+  border-radius: 10px;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  border-radius: 50%;
+  border: 2px solid gray;
+  padding: 5px;
+  margin: 30px;
+`;
+
+const GithubButton = styled.a`
+  text-decoration: none;
+  color: black;
+  border: 2px solid lightgray;
+  border-radius: 10px;
+  padding: 2px 5px;
+  :hover {
+    color: white;
+    border: 2px solid black;
+    background-color: black;
+  }
+`;
+
+const InfoContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  color: white;
+`;
+
+const InfoBox = styled.div`
+  width: 130px;
+  text-align: center;
+  padding: 5px;
+  font-size: 1rem;
+  div {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+`;
 
 export default ResultCard;
