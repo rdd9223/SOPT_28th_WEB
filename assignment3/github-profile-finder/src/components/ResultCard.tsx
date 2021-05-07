e98e4a314666f52957ebabedf5255ddbf8b5559d
YeongWoooo <rdd9223@gmail.com>
2021-05-07 23:47:02 +0900

    Style: 사용하지 않는 클래스명 삭제

--- assignment3/github-profile-finder/src/components/ResultCard.tsx
+++ assignment3/github-profile-finder/src/components/ResultCard.tsx
@@ -4,7 +4,7 @@ import { IResultCardProps } from "../interfaces";
 const ResultCard = ({ data }: IResultCardProps) => {
   return (
     <>
-      <div className="result_card">ResultCard</div>
+      <div>ResultCard</div>
       <div>
         <img src={data?.avatar_url} alt="user" />
         <div>{data?.name}</div>
