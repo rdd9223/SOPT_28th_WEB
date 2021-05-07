219ab6b718e15d8c866928ef03a3efbfa2fcf482
YeongWoooo <rdd9223@gmail.com>
2021-05-08 01:09:51 +0900

    Feat: 검색 결과 데이터 html 작성

--- assignment3/github-profile-finder/src/components/ResultCard.tsx
+++ assignment3/github-profile-finder/src/components/ResultCard.tsx
@@ -3,13 +3,27 @@ import { IResultCardProps } from "../interfaces";
 
 const ResultCard = ({ data }: IResultCardProps) => {
   return (
-    <>
-      <div>ResultCard</div>
+    <div>
+      <img src={data?.avatar_url} alt="user" />
+      <div>{data?.name}</div>
+      <div>{data?.login}</div>
+      <div>{data?.bio}</div>
+      <a href={data?.html_url}>Visit GitHub</a>
       <div>
-        <img src={data?.avatar_url} alt="user" />
-        <div>{data?.name}</div>
+        <div>
+          Followers
+          <div>{data?.followers}</div>
+        </div>
+        <div>
+          Following
+          <div>{data?.following}</div>
+        </div>
+        <div>
+          Repos
+          <div>{data?.public_repos}</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
