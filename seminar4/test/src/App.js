3f85290c22454e30ba3978e3216e68085a1c1303
YeongWoooo <rdd9223@gmail.com>
2021-05-08 16:03:02 +0900

    Feat: 라우트 설정

--- seminar4/test/src/App.js
+++ seminar4/test/src/App.js
@@ -1,5 +1,27 @@
+import Main from "./pages/Main";
+import Diary from "./pages/Diary";
+import MainHeader from "./components/common/MainHeader";
+import Calendar from "./components/common/Calender";
+import Title from "./components/common/Title";
+import Footer from "./components/common/Footer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <MainHeader />
+      <Calendar />
+      <Title />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/diary/:id" component={Diary} />
+          <Route component={() => <div>Page Not Found</div>} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ seminar4/test/src/pages/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>Mainpage</div>;
+};
+
+export default Main;
