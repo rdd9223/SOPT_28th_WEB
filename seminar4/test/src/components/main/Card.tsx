2f5a475b37c28ad1e22a7b27b49fcd7fb53e7c03
YeongWoooo <rdd9223@gmail.com>
2021-06-07 21:14:39 +0900

    Style: 사용하지 않는 주석 제거

--- seminar4/test/src/components/main/Card.tsx
+++ seminar4/test/src/components/main/Card.tsx
@@ -15,7 +15,7 @@ const CardWrap = styled.div`
     box-sizing: border-box;
     width: 220px;
     height: 257px;
-    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); // 컴포넌트에 Effects 항목을 누르면 된다. (in figma)
     border-radius: 15px;
     &:hover {
       cursor: pointer;
@@ -84,33 +84,27 @@ const CardWrap = styled.div`
 `;
 
 const Card = ({ props }: ICardProps) => {
-  // 위에서 전달한 props를 받아옵니다
-  const { date, title, image, weather, tags } = props; // 구조분해할당으로 각각 요소들을 분리해줍니다
+  const { date, title, image, weather, tags } = props;
 
   return (
     <CardWrap>
       <div className="card">
         <div className="card__image">
-          {image ? ( // userData에 image가 있으면 image를 반환하고, 없으면 아이콘을 반환합니다
+          {image ? (
             <img className="card__image--photo" src={image} alt="" />
           ) : (
             <img className="card__image--empty" src={EmptyImage} alt="" />
           )}
         </div>
         <div className="card__top">
-          {" "}
-          {/* 날짜와 날씨를 보여줍니다*/}
           <div className="card__top--date">{getDateFormat(date)}</div>
           <div className="card__top--weather">{weather}</div>
         </div>
-        <div className="card__title">{title}</div>
+        <div className="card__title">{title || "제목 없음"}</div>
         <div className="card__tags">
           {tags.map((tag: string, index: number) => {
-            // tag가 list 형식으로 저장되어있기 때문에 map을 사용합니다
             return (
               <div key={index} className="card__tags--tag">
-                {" "}
-                {/* key로 index를 사용합니다 */}
                 {tag}
               </div>
             );
